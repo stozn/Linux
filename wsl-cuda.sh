92de6ee05d3f7cd6961c4585e4af71c6caed9150
stozn <stozn.cn@outlook.com>
2025-01-20 19:41:26 +0800

    update

--- wsl-cuda.sh
+++ wsl-cuda.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-
+# 设置 CUDA repository pin
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
+# 下载CUDA Deb包
 wget https://developer.download.nvidia.com/compute/cuda/12.6.3/local_installers/cuda-repo-wsl-ubuntu-12-6-local_12.6.3-1_amd64.deb
 sudo dpkg -i cuda-repo-wsl-ubuntu-12-6-local_12.6.3-1_amd64.deb
 
+# 复制gpg密钥
 sudo cp /var/cuda-repo-wsl-ubuntu-12-6-local/cuda-*-keyring.gpg /usr/share/keyrings/
 
+# 安装CUDA
 sudo apt update
 sudo apt install cuda-toolkit-12-6 -y
 
+# 设置环境变量
 echo '
 export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 ' >> ~/.bashrc
 source ~/.bashrc
 
+# 下载安装cuDNN
 wget https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/cudnn-linux-x86_64-9.6.0.74_cuda12-archive.tar.xz
 tar -xvf cudnn-*-archive.tar.xz
 sudo cp -f cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include/
