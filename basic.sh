9528602d29f9dce4e7c6e1d1e701d0194916e843
stozn <stozn.cn@outlook.com>
2025-02-10 13:54:28 +0800

    update

--- basic.sh
+++ basic.sh
@@ -30,7 +30,7 @@ sudo apt upgrade -y
 # 安装Miniconda
 mkdir -p ~/miniconda3
 wget https://mirrors.sustech.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -h -p ~/miniconda3
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 source ~/miniconda3/bin/activate
 conda init --all
