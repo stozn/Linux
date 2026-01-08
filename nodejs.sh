c25ac84a16618c7d7ceb4816488c87c9a24e7e4c
stozn <stozn.cn@outlook.com>
2026-01-08 22:05:17 +0800

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 安装nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/refs/heads/master/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
